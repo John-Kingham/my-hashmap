3fe8510d8160507032c9fa3ea7c031bde3658cb6
John Kingham <196238029+John-Kingham@users.noreply.github.com>
2025-02-07 20:39:56 +0000

    minor reformatting using Black

--- hashmap_test.py
+++ hashmap_test.py
@@ -1,16 +1,17 @@
 import unittest
 from hashmap import HashMap
 
+
 class TestHashmap(unittest.TestCase):
 
     def test_assign(self):
         hash_map = HashMap(20)
-        hash_map.assign('Anne', 53)
-        hash_map.assign('Bob', 50)
-        hash_map.assign('Colin', 17)
-        self.assertEqual(hash_map.retrieve('Bob'), 50)
-        self.assertIsNone(hash_map.retrieve('Dave'))
+        hash_map.assign("Anne", 53)
+        hash_map.assign("Bob", 50)
+        hash_map.assign("Colin", 17)
+        self.assertEqual(hash_map.retrieve("Bob"), 50)
+        self.assertIsNone(hash_map.retrieve("Dave"))
 
     def test_retrieve(self):
         hash_map = HashMap(20)
-        self.assertIsNone(hash_map.retrieve('Anne'))       
+        self.assertIsNone(hash_map.retrieve("Anne"))
